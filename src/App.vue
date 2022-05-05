e45488f16cf6860b592c55067ff4f50779417597
lizilong <2791778524@qq.com>
2022-05-05 16:30:30 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,18 @@
 </template>
 
 <script>
-import { onBeforeMount, onBeforeUpdate, onMounted, onUpdated, reactive, ref, computed, onUnmounted, onBeforeUnmount, onErrorCaptured } from 'vue'
+import {
+  onBeforeMount,
+  onBeforeUpdate,
+  onMounted,
+  onUpdated,
+  reactive,
+  ref,
+  computed,
+  onUnmounted,
+  onBeforeUnmount,
+  onErrorCaptured,
+} from "vue";
 export default {
   name: "App",
   data() {
@@ -27,15 +38,15 @@ export default {
     const state = reactive({
       count: 10,
       //计算属性
-      getAddress: computed(()=>{
-        address = 'xxx'
-        return address
-      })
-    })
+      getAddress: computed(() => {
+        address = "xxx";
+        return address;
+      }),
+    });
     console.log(state.count);
     console.log(props);
     //定义响应式变量
-    const min = ref(0)
+    const min = ref(0);
     console.log(min.value);
     function say() {
       alert("点击了");
@@ -45,37 +56,37 @@ export default {
       return scoure;
     }
     function editCount() {
-      min.value = 10
+      min.value = 10;
       console.log(min);
     }
     //组件加载完成
-    onMounted(()=>{
-      console.log('组件加载完成')
-    })
+    onMounted(() => {
+      console.log("组件加载完成");
+    });
     //组件加载前
-    onBeforeMount(()=>{
-      console.log('组件加载完成前')
-    })
+    onBeforeMount(() => {
+      console.log("组件加载完成前");
+    });
     //组件更新后
-    onUpdated(()=>{
-      console.log('数据更新后')
-    })
+    onUpdated(() => {
+      console.log("数据更新后");
+    });
     //组件更新前
-    onBeforeUpdate(()=>{
-      console.log('数据更新前')
-    })
+    onBeforeUpdate(() => {
+      console.log("数据更新前");
+    });
     //组件销毁后
-    onUnmounted(()=>{
-      console.log('组件销毁后')
-    })
+    onUnmounted(() => {
+      console.log("组件销毁后");
+    });
     //组件销毁前
-    onBeforeUnmount(()=>{
-      console.log('组件销毁前')
-    })
+    onBeforeUnmount(() => {
+      console.log("组件销毁前");
+    });
     //捕获错误
-    onErrorCaptured(()=>{
-      console.log('捕获错误')
-    })
+    onErrorCaptured(() => {
+      console.log("捕获错误");
+    });
     return {
       name,
       age,
@@ -84,7 +95,7 @@ export default {
       state,
       say,
       sum,
-      editCount
+      editCount,
     };
   },
 };
